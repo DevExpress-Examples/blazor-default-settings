2147dbff38259a3973338a8465489fd17a4217e4
vladimir.zabelin@devexpress.com <vladimir.zabelin@devexpress.com>
2019-10-31 18:41:47 +0300

    add example content

---
+++ CS/DxBlazorComponentsDefaultSettings/Components/MyDataGrid.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using DevExpress.Blazor;
+
+namespace DxBlazorComponentsDefaultSettings.Components
+{
+    public class MyDataGrid<T> : DxDataGrid<T>
+    {
+        public override Task SetParametersAsync(ParameterView parameters) {
+            base.PageSize = 7;
+            base.ShowFilterRow = false;
+            base.ShowGroupPanel = true;
+            base.AllowColumnDragDrop = false;
+            return base.SetParametersAsync(parameters);
+        }
+    }
+}
---
+++ CS/DxBlazorComponentsDefaultSettings/Components/MyTextBox.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using DevExpress.Blazor;
+using Microsoft.AspNetCore.Components;
+
+namespace DxBlazorComponentsDefaultSettings.Components
+{
+    public class MyTextBox : DxTextBox
+    {
+        public override Task SetParametersAsync(ParameterView parameters) {
+            base.ReadOnly = true;
+            return base.SetParametersAsync(parameters);
+        }
+    }
+}
